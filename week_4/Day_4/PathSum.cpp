990e1ccf4fe218ce67be865dc2fd4d4051d4591c
11Aanchal <aanchalkaushik37@gmail.com>
2026-06-28 21:40:31 +0530

    Create PathSum.cpp

---
+++ week_4/Day_4/PathSum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL) return false;
+        if(root->left==NULL && root->right==NULL){
+            return (root->val == targetSum);
+        }
+        targetSum = targetSum - root->val;
+
+        return hasPathSum(root->left, targetSum) || 
+               hasPathSum(root->right, targetSum);
+    }
+};
